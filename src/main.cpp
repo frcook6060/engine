4a51cf821b5dcdd022f9ba9706ce472f9cfcc624
frcook6060 <frcook.6060@gmail.com>
2019-04-26 02:34:16 -0500

    Finished window example...

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,85 @@
 #include <iostream>
+#include <string>
+
 #include <Windows.h>
+
+#define WIN_WS_TYPE WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX
+
+static std::string winClass = "_engine_window";
+static std::string caption = "Engine Window";
+static int width = 1280;
+static int height = 720;
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPreInst, LPSTR lpCmdLine, int nCmdShow)
 {
-	MessageBox(nullptr, "Goodbye, cruel world!", "Note", MB_OK);
+	WNDCLASSEX wc;
+	HWND hwnd;
+	MSG msg;
+
+	ZeroMemory(&wc, sizeof(wc));
+
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.lpfnWndProc = WndProc;
+	wc.hInstance = hInst;
+	wc.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wc.lpszClassName = winClass.c_str();
+	wc.hIconSm = LoadIcon(nullptr, IDI_APPLICATION);
+
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(nullptr, "Window Registers Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	hwnd = CreateWindowEx(
+		WS_EX_CLIENTEDGE,
+		winClass.c_str(),
+		caption.c_str(),
+		WIN_WS_TYPE,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		width,
+		height,
+		nullptr,
+		nullptr,
+		hInst,
+		nullptr);
+
+	if (hwnd == nullptr)
+	{
+		MessageBox(nullptr, "Window Creation Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+	return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hwnd, msg, wparam, lparam);
+	}
 	return 0;
 }
